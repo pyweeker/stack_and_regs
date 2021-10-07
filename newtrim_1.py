78fb42f707f64c147ad657fbfb808b62ff1307cb
pyweeker <81862199+pyweeker@users.noreply.github.com>
2021-10-07 09:29:47 +0200

    Add files via upload

---
+++ newtrim_1.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import re
+import itertools
+
+
+oldfile = "./9stars.asm"
+#oldfile = "./hello.asm"
+#oldfile = "./shuffle.asm"
+
+
+def trimit(oldfile):
+
+    list_of_lines = list()
+
+    '''\
+    Read a list of names from a file line by line into an output file.
+    If a line begins with a particular name, insert a string of text
+    after the name before appending the line to the output file.
+    '''
+
+    with open(oldfile, 'r', encoding='utf-8') as infile:
+        for line in infile:
+            line = line.split(";")[0].strip()
+            line_lstriped = line.lstrip()
+            line_bilatstriped = line_lstriped.rstrip()
+
+
+            #print(f"--------->   {line_bilatstriped}")
+            #if line.startswith(txt):
+            notab = line_bilatstriped.replace('\t',',')
+            #full = line_bilatstriped.split(',')
+            full = notab.split(',')
+            #res = res.split(',')
+            print(f"\n\n AAAAAA--*****------->   {full}")
+            
+
+            
+
+            
+
+
+
+returned_list_of_lines = trimit(oldfile)
+
+print(len(returned_list_of_lines))
+
+print(returned_list_of_lines)
